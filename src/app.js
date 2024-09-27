63ed7b99c0e3792b61427a0cd2d2e2461afd5e7a
Brian Weck <122367+bweck@users.noreply.github.com>
2024-09-27 11:16:08 +1000

    Add charter type refinement Move hit rendering template

--- src/app.js
+++ src/app.js
@@ -13,53 +13,12 @@ import {
   stats,
 } from 'instantsearch.js/es/widgets'
 import { renderDateBeforeInput, renderDateAfterInput } from './dateInput'
-import { DateTime } from 'luxon'
+import { renderHit } from './renderHit'
 
 //
 const ALGOLIA_APP_ID = 'XS4X4GJGA8'
 const ALGOLIA_API_KEY = '1b6bee0488dc899e58e57f748383e757'
 const ALGOLIA_INDEX_NAME = 'yachts'
-
-//
-// setup some templates to use for rendering the search results.
-const TEMPLATE_EMPTY_RESULTS = '<div class="text-center">No results found.</div>'
-// render the hit information (sorry for the messy code here)
-const TEMPLATE_HIT = (hit, html, components) => html`
-  <div class="flex flex-col pb-4">
-    <div class="text-sky-700 text-lg">
-      ${components.Highlight({ hit, attribute: 'name' })}
-      <span class="text-gray-300 pl-8 text-sm">${hit.uri}</span>
-    </div>
-    <div>${components.Highlight({ hit, attribute: 'description' })}</div>
-    <div class="hidden">Hit: <pre>${JSON.stringify(hit, null, 2)}</pre></div>
-    <div>Sleeps: ${hit.sleeps} | Cabins: ${hit.cabins} | Bathrooms: ${hit.bathrooms}</div>
-    <div>
-      <!-- render information from our mock backend -->
-      <ol class="pl-4">
-        ${hit.mockBackend.pricing.pricingInfo.map((pricingInfo) => {
-          return pricingInfo.effectiveDates.map((effectiveDate) => {
-            return html`<li class="pt-2">
-              →
-              <span> From ${DateTime.fromISO(effectiveDate.from).toISODate()}</span>
-              <span> to </span>
-              <span>${DateTime.fromISO(effectiveDate.to).toISODate()}</span>
-              <ul class="list-disc pl-8">
-                <li>Charter Type: ${pricingInfo.pricing.unit == 'HOUR' ? 'DAY' : 'TERM'}</li>
-                <li>Price: ${pricingInfo.pricing.currency} ${pricingInfo.pricing.total / 100}</li>
-                <li>
-                  Locations:
-                  <ol class="list-decimal pl-4">
-                    ${pricingInfo.pricing.inclusionZones.map((zone) => html`<li>${zone.category.join(' > ')}</li>`)}
-                  </ol>
-                </li>
-              </ul>
-            </li>`
-          })
-        })}
-      </ol>
-    </div>
-  </div>
-`
 
 // setup the search client.
 const searchClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY)
@@ -98,30 +57,17 @@ const widgets = [
 
   //
   // refinement - charter type
-  refinementList({
+  menuSelect({
     container: '#refinement-charterType',
     attribute: 'charterType',
-    sortBy: ['name:asc'],
-    operator: 'or',
-    showMore: false,
-    searchable: false,
-    cssClasses: {
-      root: 'w-full',
-      list: 'flex justify-center gap-x-2',
-    },
     templates: {
-      item(item, { html }) {
-        const { url, label, count, isRefined } = item
-        return html`
-          <button
-            class="inline-flex items-center justify-center rounded-full w-24 ${isRefined
-              ? 'bg-sky-300'
-              : 'bg-gray-100'}"
-          >
-            <span>${label}</span><span class="pl-1 text-sm text-gray-400">(${count})</span>
-          </button>
-        `
+      defaultOption(data, { html }) {
+        return html`<span>DAY & TERM</span>`
       },
+    },
+    cssClasses: {
+      select:
+        'bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
     },
   }),
 
@@ -329,8 +275,8 @@ const widgets = [
   hits({
     container: '#hits',
     templates: {
-      empty: TEMPLATE_EMPTY_RESULTS,
-      item: (hit, { html, components }) => TEMPLATE_HIT(hit, html, components),
+      empty: '<div class="text-center">No results found.</div>',
+      item: (hit, { html, components }) => renderHit(hit, { uiState: search.getUiState(), html, components }),
     },
   }),
 ]
