11a77fa3a88a802e0524d503a1cbc1558d978288
Brian Weck <122367+bweck@users.noreply.github.com>
2024-10-02 01:59:52 +1000

    move functions to global context

--- src/yacht-template.js
+++ src/yacht-template.js
@@ -1,7 +1,29 @@
-
 // import the sample data and expose it to the global scope to be
 // used by the template page.
 import ankorAPIResponse from '../data/bigsky.json'
 global.ankorAPIResponse = ankorAPIResponse
 
+// Convert the path provided by the API to a full URL
+global.toImageUrl = function (loc, imageVariant) {
+  const _loc = loc.replace('{imageVariant}', imageVariant)
+  return `https://api.ankor.io${_loc}`
+}
 
+// format a price
+global.formatPrice = function (p) {
+  return Intl.NumberFormat('en-US', {
+    style: 'currency',
+    notation: 'compact',
+    currency: p.currency,
+  }).format(p.price / 100)
+}
+
+// format a date
+global.formatDate = function (d) {
+  console.log(d)
+  return new Date(d).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  })
+}
